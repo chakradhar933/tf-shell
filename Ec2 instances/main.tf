16bb0ba1e78e641d587fd5fbd66a406d0d6f7cb1
ram <chakradhar.thatikonda933@gmail.com>
2023-01-19 11:31:26 +0530

    terraform coe

---
+++ Ec2 instances/main.tf
@@ -0,0 +1,17 @@
+
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "project-tf-1"
+  }
+}
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "project-tf-2"
+  }
+}
---
+++ Ec2 instances/variables.tf
@@ -0,0 +1,12 @@
+variable "ami" {
+  default = "ami-0ab0629dba5ae551d"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "ami" {
+  default = "ami-0ab0629dba5ae551d"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
